41546e38272e723d0443dcf6a2f94927471b71dc
Tim J. K <tim.kicker@student.htl-rankweil.at>
2022-02-21 15:19:18 +0100

    init

---
+++ MeasurementApplication/CLI_MeasurementApplication/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace MyApp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //set variables MANUALLY
+            int baudRate = 16000000;
+            StopBits stopBits = StopBits.None;
+            Parity parity = Parity.None;
+            Handshake portHandshake = Handshake.None;
+
+            int readTimeout = 500;
+            int writeTimeout = 500;
+
+            //initialize port
+            SerialPort devicePort = new SerialPort();
+            devicePort.BaudRate = baudRate;
+            devicePort.Parity = parity;
+            devicePort.StopBits = stopBits;
+            devicePort.Handshake = portHandshake;
+            devicePort.ReadTimeout = readTimeout;
+            devicePort.WriteTimeout = writeTimeout;
+
+            //try open port
+            try
+            {
+                devicePort.Open();
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Error OPENING port!");
+            }
+
+            //try reading port
+            readPortLoop(devicePort);
+        }
+
+        public static void readPortLoop( SerialPort devicePort)
+        {
+            while (Console.ReadLine() != "q")
+            {
+                try
+                {
+                    string message = devicePort.ReadLine();
+                    Console.WriteLine(message);
+                }
+                catch (TimeoutException) { Console.WriteLine("Error REDING port!"); }
+            }
+        }
+    }
+}
