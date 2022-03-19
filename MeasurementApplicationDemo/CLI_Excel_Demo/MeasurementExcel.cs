260be1d5b7e79255ad934c4d5938c3578cd32686
Tim J. K <tim.kicker@student.htl-rankweil.at>
2022-03-19 16:43:26 +0100

    [GUI] Excel export demo
    - add test application
    - add static class for export

---
+++ MeasurementApplicationDemo/CLI_Excel_Demo/MeasurementExcel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLI_Excel_Demo
+{
+    public class Measurement
+    {
+        private int _value;
+        private DateTime _timeStamp;
+        private const char SEP = ',';
+
+        public Measurement(int value)
+        {
+            _timeStamp = DateTime.UtcNow;
+            _value = value;
+        }
+
+        public string Serialize()
+        {
+            return
+                $"German{SEP}{_timeStamp}{SEP}{_value}{SEP}";
+        }
+
+        public int Value { get => _value; }
+        public DateTime TimeStamp { get => _timeStamp; }
+    }
+}
---
+++ MeasurementApplicationDemo/CLI_Excel_Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CLI_Excel_Demo
+{
+    public class Program
+    {
+        
+
+        static async Task Main(string[] args)
+        {
+            var _measList = GetMeasurements();
+            var _fileInfo = new FileInfo(@"C:\Users\Tim\Desktop\Test.xlsx");
+
+            await ExcelConverter.ExportAsync(_fileInfo, _measList);
+        }
+
+        public static ObservableCollection<Measurement> GetMeasurements()
+        {
+            var measList = new ObservableCollection<Measurement>();
+
+            for (int i = 0; i < 11; i++)
+            {
+                var newMeas = new Measurement(i);
+                measList.Add(newMeas);
+            }
+
+            return measList;
+        }
+    }
+}
