414c283add8124381d97c7ab4d786ef9cb7cc1f4
botchGNU <kickers.world.tim@gmail.com>
2022-03-29 10:38:28 +0200

    add export command

---
+++ MeasurementApplicationDemo/MeasurementApplicationDemo/Command/ExportCommand.cs
@@ -0,0 +1,26 @@
+﻿using CLI_Excel_Demo;
+using GUI_Meas_Demo.Model;
+using GUI_Meas_Demo.ViewModel;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace GUI_Meas_Demo.Command
+{
+    internal class ExportCommand : CommandBase
+    {
+        private ObservableCollection<Measurement> _measurements;
+
+        public ExportCommand(ObservableCollection<Measurement> measurements)
+        {
+            _measurements = measurements;
+        }
+
+        public override void Execute(object parameter)
+        {
+            ExportManager.AsExcelAsync(_measurements);
+        }
+    }
+}
---
+++ MeasurementApplicationDemo/MeasurementApplicationDemo/Model/ExportManager.cs
@@ -0,0 +1,24 @@
+﻿using CLI_Excel_Demo;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
+
+namespace GUI_Meas_Demo.Model
+{
+    public static class ExportManager
+    {
+        public static async void AsExcelAsync(ObservableCollection<Measurement> measList)
+        {
+            SaveFileDialog savefile = new SaveFileDialog();
+            savefile.FileName = "Measurement.xlsx";
+            savefile.Filter = "Excel Sheet (*.xlsx)|*.txt|All files (*.*)|*.*";
+
+            savefile.ShowDialog();
+
+            await ExcelConverter.ExportAsync(new FileInfo(savefile.FileName), measList);
+        }
+    }
+}
