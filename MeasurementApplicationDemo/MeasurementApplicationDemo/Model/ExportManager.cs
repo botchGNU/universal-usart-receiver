fadba9dc54264aba84c395220c1abe42f5c32b7d
Tim Kicker <tim.kicker@student.htl-rankweil.at>
2022-05-06 16:51:50 +0200

    export as binary task

--- MeasurementApplicationDemo/MeasurementApplicationDemo/Model/ExportManager.cs
+++ MeasurementApplicationDemo/MeasurementApplicationDemo/Model/ExportManager.cs
@@ -2,20 +2,67 @@
 using Microsoft.Win32;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Text.Json;
 
 namespace GUI_Meas_Demo.Model
 {
     public static class ExportManager
     {
-        public static async void AsExcelAsync(ObservableCollection<Measurement> measList)
+        public static async void ExportAsync(ObservableCollection<Measurement> measList)
         {
             SaveFileDialog savefile = new SaveFileDialog();
             savefile.FileName = "Measurement.xlsx";
-            savefile.Filter = "Excel Sheet (*.xlsx)|*.txt|All files (*.*)|*.*";
+            savefile.Filter = "Excel Sheet (*.xlsx)|*.xlsx|Binary (*.bin)|*.bin|All files (*.*)|*.*";
 
-            savefile.ShowDialog();
+            if (savefile.ShowDialog() == true)
+            {
+                string fileExt = System.IO.Path.GetExtension(savefile.FileName);
 
-            await ExcelConverter.ExportAsync(new FileInfo(savefile.FileName), measList);
+                switch (fileExt.ToLower())
+                {
+                    case ".csv":
+                        //not implemented yet
+                        break;
+
+                    case ".json":
+                        //not implemented yet
+                        break;
+
+                    case ".xlsx":
+                        await ExcelConverter.ExportAsync(new FileInfo(savefile.FileName), measList);
+                        break;
+
+                    case ".bin":
+                    case ".address":
+                        await ExportBinAsync(new FileInfo(savefile.FileName), measList);
+                        break;
+
+                    case ".db":
+                    case ".sqlite":
+                        //not implemented yet
+                        break;
+                    default:
+                        break;
+                }
+
+            }  
         }
+
+
+
+        private static async Task ExportBinAsync(FileInfo fileName, ObservableCollection<Measurement> measColl)
+        {
+            Stream str = File.OpenWrite(fileName.ToString());
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(str, measColl);
+            str.Close();
+        }
+
+
     }
 }
