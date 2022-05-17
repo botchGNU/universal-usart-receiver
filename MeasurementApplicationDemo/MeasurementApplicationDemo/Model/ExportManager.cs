66bae54ede5e442a58d880932400730edf1423e8
botchGNU <kickers.world.tim@gmail.com>
2022-05-17 10:43:13 +0200

    update export
    - timestamp in filename
    - open file on export
    - add icon

--- MeasurementApplicationDemo/MeasurementApplicationDemo/Model/ExportManager.cs
+++ MeasurementApplicationDemo/MeasurementApplicationDemo/Model/ExportManager.cs
@@ -1,13 +1,14 @@
 ﻿using CLI_Excel_Demo;
 using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
 using System.Collections.ObjectModel;
+using System.Data.SQLite;
+using System.Diagnostics;
 using System.IO;
-using System.Threading.Tasks;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
-using System.Text.Json;
 
 namespace GUI_Meas_Demo.Model
 {
@@ -16,8 +17,16 @@ namespace GUI_Meas_Demo.Model
         public static async void ExportAsync(ObservableCollection<Measurement> measList)
         {
             SaveFileDialog savefile = new SaveFileDialog();
-            savefile.FileName = "Measurement.xlsx";
-            savefile.Filter = "Excel Sheet (*.xlsx)|*.xlsx|Binary (*.bin)|*.bin|All files (*.*)|*.*";
+            string filenameDate = DateTime.Now.Day + "-" + 
+                                  DateTime.Now.Month + "-" + 
+                                  DateTime.Now.Year + "_" + 
+                                  DateTime.Now.Hour + "-" +
+                                  DateTime.Now.Minute + "-" +
+                                  DateTime.Now.Second + "_" +
+                                  "_Measurement.xlsx";
+
+            savefile.FileName = filenameDate;
+            savefile.Filter = "Excel Sheet (*.xlsx)|*.xlsx|Binary (*.bin)|*.bin|CSV (*.csv)|*.csv|Database SQLite (*.db)|*.db|XAML (*.xaml)|*.xaml|JSON (*.json)|*.json";
 
             if (savefile.ShowDialog() == true)
             {
@@ -25,44 +34,185 @@ namespace GUI_Meas_Demo.Model
 
                 switch (fileExt.ToLower())
                 {
-                    case ".csv":
-                        //not implemented yet
+                    case ".csv":    //not tested
+                        await ExportCSVAsync(new FileInfo(savefile.FileName), measList);
                         break;
 
                     case ".json":
-                        //not implemented yet
+                        await ExportJSONAsync(new FileInfo(savefile.FileName), measList);
                         break;
 
-                    case ".xlsx":
+                    case ".xaml":
+                        await ExportXAMLAsync(new FileInfo(savefile.FileName), measList);
+                        break;
+
+                    case ".xlsx":   //not working ATM
                         await ExcelConverter.ExportAsync(new FileInfo(savefile.FileName), measList);
                         break;
 
                     case ".bin":
-                    case ".address":
+                    case ".binary":
                         await ExportBinAsync(new FileInfo(savefile.FileName), measList);
                         break;
 
                     case ".db":
                     case ".sqlite":
-                        //not implemented yet
+                        await ExportDatabaseAsync(savefile.FileName, measList);
                         break;
                     default:
                         break;
                 }
 
-            }  
+            }
+            OpenFolderOnFile(savefile.FileName);
         }
 
+        private static void OpenFolderOnFile(string folderPath)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            string _path = folderPath;
+            startInfo.Arguments = string.Format("/C start {0}", _path);
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+
+        
+        private static SQLiteConnection CreateSQLiteConnection(string fileName)
+        {
+            SQLiteConnection conn = new SQLiteConnection($"Data Source={fileName}");
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception exp)
+            {
+                throw exp;
+            }
+
+            return conn;
+        }
+        
+        #region export Tasks
+        public static async Task ExportXAMLAsync(FileInfo fileName, ObservableCollection<Measurement> measList)
+        {
+            try
+            {
+                XmlSerializer writer = new XmlSerializer(typeof(ObservableCollection<Measurement>));
+
+                using (StreamWriter file = new StreamWriter(fileName.ToString()))
+                {
+                    writer.Serialize(file, measList);
+                    file.Close();
+                }
+                Notification.PrintFileMessage(true, fileName.Name);
+            }
+            catch (Exception)
+            {
+                Notification.PrintFileMessage(false, fileName.Name);
+            }
+
+        }
 
 
         private static async Task ExportBinAsync(FileInfo fileName, ObservableCollection<Measurement> measColl)
         {
-            Stream str = File.OpenWrite(fileName.ToString());
-            BinaryFormatter formatter = new BinaryFormatter();
-            formatter.Serialize(str, measColl);
-            str.Close();
+            try
+            {
+                Stream str = File.OpenWrite(fileName.ToString());
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(str, measColl);
+                str.Close();
+                Notification.PrintFileMessage(true, fileName.Name);
+            }
+            catch (Exception)
+            {
+
+                Notification.PrintFileMessage(false, fileName.Name);
+            }
         }
 
+        private static async Task ExportJSONAsync(FileInfo fileName, ObservableCollection<Measurement> measColl)
+        {
+            try
+            {
+                Newtonsoft.Json.JsonSerializer writer = new Newtonsoft.Json.JsonSerializer();
+                writer.Formatting = Formatting.Indented;
+
+                using (StreamWriter wfile = new StreamWriter(fileName.ToString()))
+                {
+                    writer.Serialize(wfile, measColl);
+                    wfile.Close();
+                }
+                Notification.PrintFileMessage(true, fileName.Name);
+            }
+            catch (Exception)
+            {
+                Notification.PrintFileMessage(false, fileName.Name);
+            }
+
+
+        }
+
+        private static async Task ExportCSVAsync(FileInfo fileName, ObservableCollection<Measurement> measColl)
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter(fileName.ToString());
+
+                writer.WriteLine(
+                        "TimeStamp,Value");
+                foreach (var item in measColl)
+                {
+                    writer.WriteLine(item.Serialize());
+                }
+                writer.Close();
+                Notification.PrintFileMessage(true, fileName.Name);
+            }
+            catch (Exception)
+            {
+                Notification.PrintFileMessage(false, fileName.Name);
+            }
+        }
+        private static async Task ExportDatabaseAsync(string filename, ObservableCollection<Measurement> measColl)
+        {
+
+
+
+            try
+            {
+                SQLiteConnection connection = CreateSQLiteConnection(filename);
+
+                SQLiteCommand cmd = new SQLiteCommand(connection);
+                cmd.CommandText = "DROP TABLE IF EXISTS measurements";
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText = @"CREATE TABLE measurements(id INTEGER PRIMARY KEY,
+                              timestamp TEXT, value INT)";
+                cmd.ExecuteNonQuery();
+
+                foreach (Measurement item in measColl)
+                {
+                    cmd.CommandText = $"INSERT INTO " +
+                                    $"measurements(timestamp, value) " +
+                                    $"VALUES('{item.TimeStamp}', {(int)item.Value})";
+                    cmd.ExecuteNonQuery();
+                }
+
+                connection.Close();
+                Notification.PrintFileMessage(true, filename);
+            }
+            catch (Exception)
+            {
+                Notification.PrintFileMessage(false, filename);
+            }
+
+
+        }
+        #endregion
 
     }
 }
--- README.md
+++ README.md
@@ -1,33 +1,84 @@
-# RS232 Measurement Receiver
+# Universal USART Measurement Receiver
 
-Tool in order to receive and save measurement data over RS232
-
-## Features
-- [X] Receive measurement data over RS232
-- [X] Pause session
-- [X] Error- & info notifications
-- [X] Export as Microsoft Excel file
-- [X] Excel Chart
+A universal GUI measurement tool for various devices data via USART
 
 ## Usage
 
 1. Run application
 2. Plug in your measurement device (driver may be needed)
-3. Import device settings (as json)
+3. Import device settings (see below)
 4. Start measurement
+5. Export as... (see below)
+   1. Excel + Graph
+   2. Xaml
+   3. JSON
+   4. SQLite Database
+   5. Binary
+   6. CSV
 
-## Bugs
-- [ ] Memory Leak
-- [ ] Redesign chart
 
+## How do I set up the application for my device?
+This tool uses json files in order to store device-settings. The following is an example for the device settings used for testing.
 
+````json
+{
+  "BaudRate": 57600,
+  "StopBits": 1,
+  "Parity": 0,
+  "Handshake": 0
+}
+````
 
-## Used 
+These setting-files can be imported in the 2nd step of the startup-configuration
+
+## Export Examples
+
+- Excel
+
+  ![](Documentation\Images\excel.png)
+
+- CSV
+
+  ![](Documentation\Images\csv.png)
+
+- SQLite Database
+
+  ![](Documentation\Images\sqlite.png)
+  
+- JSON
+
+  ````json
+  [
+    {
+      "TimeStamp": "30:711",
+      "Value": 157
+    },
+    {
+      "TimeStamp": "30:711",
+      "Value": 157
+    },
+    {
+      "TimeStamp": "30:711",
+      "Value": 161
+    },
+    {
+      "TimeStamp": "30:711",
+      "Value": 167
+    },
+    {
+      "TimeStamp": "30:711",
+      "Value": 175
+    },
+    {
+      "TimeStamp": "30:711",
+      "Value": 185
+    }
+  ]
+  ````
+
+## Used libraries
 
 - [.NET Core 3.1](https://dotnet.microsoft.com/en-us/download/dotnet/3.1)
 - [WPF](https://docs.microsoft.com/de-de/visualstudio/designers/getting-started-with-wpf?view=vs-2022)
 - [HandyControls](https://github.com/ghost1372/HandyControls)
 - [Notifications.Wpf](https://github.com/Federerer/Notifications.Wpf)
-
-
-​	
